b290d379f6297a0511f797262a8494ddb5bb4791
Igor Minayeu <igor.minayeu@gmail.com>
2022-06-20 20:10:48 +0300

    add props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -11,10 +11,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={'Hi, how are you?'} likesCount={15}/>
+                <Post message={"It's my first post"} likesCount={10}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+type PostPropsType = {
+    message: string
+    likesCount: number
+}
+
+const Post = (props:PostPropsType) => {
     return (
         <div className={s.item}>
             <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6vjzwZaBf6bKmbl7I00WbZ9RPOlriawksgQ&usqp=CAU"
                 alt=""/>
-            post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>{props.likesCount}</span>
             </div>
 
         </div>
