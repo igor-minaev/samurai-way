1bd781dc17a316b4ac260acb317a5545df2150dc
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-05 15:00:29 +0300

    add routing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,20 +4,20 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={s.nav}>
+            <div className={s.item}>
+                <a href='/profile'>Profile</a>
+            </div>
             <div className={`${s.item} ${s.active}`}>
-                <a>Profile</a>
+                <a href='/dialogs'>Messages</a>
             </div>
             <div className={s.item}>
-                <a>Messages</a>
+                <a href='/news'>News</a>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <a href='/music'>Music</a>
             </div>
             <div className={s.item}>
-                <a>Music</a>
-            </div>
-            <div className={s.item}>
-                <a>Settings</a>
+                <a href='/settings'>Settings</a>
             </div>
         </nav>
     )
