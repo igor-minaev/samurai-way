51dd96aacd49e3940fe82c8e651fc6b84c7f7f94
Igor Minayeu <igor.minayeu@gmail.com>
2022-07-07 14:21:05 +0300

    lesson 28 complete

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -7,12 +7,6 @@ type MyPostsPropsType = {
     posts: Array<PostType>
 }
 const MyPosts: React.FC<MyPostsPropsType> = ({posts}) => {
-    // let posts = [
-    //     {id: 1, message: 'Hi, how are you?', likesCount: 12},
-    //     {id: 2, message: 'It\'s my first post', likesCount: 11},
-    //     {id: 3, message: 'Blabla', likesCount: 11},
-    //     {id: 4, message: 'Dada', likesCount: 11}
-    // ]
 
     let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
