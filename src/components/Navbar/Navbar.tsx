23e1e8de86ad72964c74c183c604078e8c99175e
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-09 20:39:43 +0300

    lesson 19

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,9 +11,15 @@ export const Navbar = () => {
             <div className={s.item}>
                 <a href='/dialogs'>Messages</a>
             </div>
-            <div className={s.item}>News</div>
-            <div className={s.item}>Music</div>
-            <div className={s.item}>Settings</div>
+            <div className={s.item}>
+                <a href='/news'>News</a>
+            </div>
+            <div className={s.item}>
+                <a href='/music'>Music</a>
+            </div>
+            <div className={s.item}>
+                <a href='/settings'>Settings</a>
+            </div>
         </nav>
     );
 };
