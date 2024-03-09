df4587832b8aa8f1e1f666a1c22f5a260f8c349b
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-09 20:20:35 +0300

    start lesson 19

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,8 +5,12 @@ import s from './Navbar.module.css'
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={`${s.item} ${s.active}`}>Profile</div>
-            <div className={s.item}>Messages</div>
+            <div className={`${s.item} ${s.active}`}>
+                <a href='/profile'>Profile</a>
+            </div>
+            <div className={s.item}>
+                <a href='/dialogs'>Messages</a>
+            </div>
             <div className={s.item}>News</div>
             <div className={s.item}>Music</div>
             <div className={s.item}>Settings</div>
