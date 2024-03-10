79723586eaac8bb12da90e94e74404b0bb323419
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-10 18:33:57 +0300

    start lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,31 @@ import s from './Dialogs.module.css'
 export const Dialogs = () => {
     return (
         <div>
-            Dialogs
+           <div className='dialogs-items'>
+               <div className='dialog'>
+                   Dimych
+               </div>
+               <div className='dialog'>
+                   Andrey
+               </div>
+               <div className='dialog'>
+                   Sveta
+               </div>
+               <div className='dialog'>
+                   Sasha
+               </div>
+               <div className='dialog'>
+                   Viktor
+               </div>
+               <div className='dialog'>
+                   Valera
+               </div>
+           </div>
+            <div className='messages'>
+                <div className="message"></div>
+                <div className="message"></div>
+                <div className="message"></div>
+            </div>
         </div>
     );
 };
