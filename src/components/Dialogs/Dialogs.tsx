e28e9c362f51652e4d28878fd70302bc3a2a326d
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-10 18:44:47 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,31 +3,31 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-           <div className='dialogs-items'>
-               <div className='dialog'>
-                   Dimych
-               </div>
-               <div className='dialog'>
-                   Andrey
-               </div>
-               <div className='dialog'>
-                   Sveta
-               </div>
-               <div className='dialog'>
-                   Sasha
-               </div>
-               <div className='dialog'>
-                   Viktor
-               </div>
-               <div className='dialog'>
-                   Valera
-               </div>
-           </div>
-            <div className='messages'>
-                <div className="message"></div>
-                <div className="message"></div>
-                <div className="message"></div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Yo</div>
             </div>
         </div>
     );
