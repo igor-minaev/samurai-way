97cca58d9d66440d7000e14c567b6942a38c4c5e
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-10 18:47:10 +0300

    start lesson 22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Dimych
+                    <NavLink>Dimych</NavLink>
                 </div>
                 <div className={s.dialog}>
                     Andrey
